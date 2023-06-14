5d1d2a4ff65fc1aad0777d83fc59f52516692b02
Echo <98044070+MopNop@users.noreply.github.com>
2023-06-14 21:20:39 +0000

    adfg

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Particles.js configuration
+    particlesJS("particles-js", {
+      particles: {
+        // Particle options
+        // ...
+      },
+      interactivity: {
+        // Interactivity options
+        // ...
+      },
+      // Other configuration options
+      // ...
+    });
+  });
+  
